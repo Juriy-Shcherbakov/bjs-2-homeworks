6b760b1990ba3283d68a258f323711554cac3dd0
Juriy-Shcherbakov <t9296298804@gmail.com>
2022-06-12 12:12:17 +0300

    solution added in to file task.js folder 4.objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,41 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+const student1 = new Student('Tom', 'male', '23');    
+const student2 = new Student('Alex', 'male', '24'); 
+const student3 = new Student('Oleg', 'male', '25'); 
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [];
+  } 
+  this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function (...marksArr) {
+  if (this.marks === undefined) { 
+    this.marks = [];
+  } 
+  marksArr.forEach(marksArr => this.marks.push(marksArr));
+} 
+
+Student.prototype.getAverage = function () {
+let average = 0;
+  if (!(this.marks === undefined)) {
+    this.marks.forEach((marks) => average += marks);
+    return average / this.marks.length; 
+  }
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+}
